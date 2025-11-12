3cd51992073915859bffa77e8d5f63aae1a8c6a5
PriyankaR5319 <152068115+PriyankaR5319@users.noreply.github.com>
2025-11-12 11:44:05 +0530

    Update jenkins-server.sh

--- jenkins-server.sh
+++ jenkins-server.sh
@@ -6,9 +6,9 @@
 ################################################################################################
 
 apt update -y  # It will update repo 
-apt install openjdk-21-jre -y # It will install Openjdk
+apt install openjdk-17-jre -y # It will install Openjdk
 
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2025.key | sudo tee \
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null  # This will update the repository for jenkins
 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
